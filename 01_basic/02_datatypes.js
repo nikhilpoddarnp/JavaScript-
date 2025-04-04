8b0061f60a4e88db6b03d903e9d71d908ba69369
Nikhil <nikhlrajkumarjee3@gmail.com>
2025-04-04 11:05:19 +0530

    datatype

---
+++ 01_basic/02_datatypes.js
@@ -0,0 +1,43 @@
+"use strict" ; // treat all JS code as newer version
+
+// alert ( 3 + 3) // we are using nodejs , not browser
+
+console.log( 3
+    +
+    3
+) // code readability should be high
+
+console.log("Nikhil"); 
+
+
+let name = "Nikhil"
+let age = 23
+let isLoggedIn = false
+
+//datatype -> 
+
+// 1. Primitive datatype
+
+// number  => 2 to the power 53
+// bigint 
+// string => ""
+// boolean => true/false
+// null => standalone value / representation of empety value ex. let balence = null
+// undefine =>
+// symbol  => unique 
+
+
+// 2. Non-primitive datatype
+
+// object 
+
+
+
+console.log(typeof age); // it shows number 
+// and one more thing typeof undefine is undefine.
+
+console.log(typeof null);  //it shows object it means null is object.
+
+
+
+ 
