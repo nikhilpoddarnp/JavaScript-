a68240d66b90d43d06943003bf5310bd517e657c
Nikhil <nikhlrajkumarjee3@gmail.com>
2025-04-10 12:59:10 +0530

    comparison

---
+++ 01_basic/04_comparison.js
@@ -0,0 +1,25 @@
+console.log(2 > 1);  //true
+console.log(2 >= 1);  //true
+console.log(2 < 1);  //false
+console.log(2 <= 1);  //false
+console.log(2 != 1);  //true
+
+
+console.log("2" != 1);  //true
+console.log("2" > 1);  //true
+
+// for equal to comparison we use 
+// 1.  == : this  compare only the values and convert the type
+// 2.  === : this compare values and type both .(Strict Comparison)
+
+
+// we avoid this type of conversion
+
+console.log(null > 0);
+console.log(null >= 0);
+console.log(null == 0);
+
+console.log(undefined > 0);
+console.log(undefined == 0);
+console.log(undefined >= 0);
+
