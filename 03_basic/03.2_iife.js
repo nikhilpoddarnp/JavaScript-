7b629fa8cb144ded1aa84c893b403fae9dcd2815
Nikhil <nikhlrajkumarjee3@gmail.com>
2025-04-13 15:20:01 +0530

    iffe

--- 03_basic/03.2_iife.js
+++ 03_basic/03.2_iife.js
@@ -1,13 +1,10 @@
 // Immediately Invoked Function Expressions (IIFE)
 
+(function chai() {
+  console.log(`DB CONNECTED`); //DB CONNECTED
+})(); //HERE WE HAVE TO USE ;(SEMI COLON) TO END THIS FUNCTION
+//if we don't use ; the afther the no one function will execute
 
-(function chai(){
-    console.log(`DB CONNECTED`);  //DB CONNECTED
-    
-})();  //HERE WE HAVE TO USE ;(SEMI COLON) TO END THIS FUNCTION 
-//if we don't use ; the afther the no one function will execute 
-
-( () => {
-    console.log(`2ND DB CONNECTED`); //2ND DB CONNECTED
-    
-} ) ()
+((name) => {
+  console.log(`2ND DB CONNECTED ${name}`); //2ND DB CONNECTED mohit
+})('mohit');
