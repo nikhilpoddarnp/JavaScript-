68e40bf99b62237b9a2114676aaedd45ed83d630
Nikhil <nikhlrajkumarjee3@gmail.com>
2025-04-14 00:48:42 +0530

    switch

---
+++ 04_control_flow/02_switch.js
@@ -0,0 +1,36 @@
+//we use in redux 
+
+//basic sintex of switch
+/*
+const key = value;
+switch (key) {
+    case value:
+        
+        break;
+
+    default:
+        break;
+}
+*/
+const month = 3
+
+switch(month){
+    case 1 :
+    console.log("january");
+    break;
+    case 2 :
+        console.log("feb");
+        break;
+        case 3 :
+            console.log("march");
+        break;
+    case 4:
+    console.log("april");
+    break;
+    
+    default :
+    console.log("default caes match");
+    break;
+          
+} //output : march
+
