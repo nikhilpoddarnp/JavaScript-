bcc7bbd135ca96a67cdb2737adb5890421a3965b
Nikhil <nikhlrajkumarjee3@gmail.com>
2025-04-14 12:32:59 +0530

    while and do-while

---
+++ 05_iteration_or_loop/02_do_while.js
@@ -0,0 +1,24 @@
+//while 
+
+let i =0
+while (i<=10) {
+   console.log(i);
+   i+=2
+    
+}
+
+let myrr=['nikhil','raja','mohit']
+arr=0
+while(arr< myrr.length){
+    console.log(`name : ${myrr[arr]}`);
+    arr++
+}
+
+//do while
+
+let score=0
+do{
+console.log(score);
+
+score++
+}while (score<=12)
